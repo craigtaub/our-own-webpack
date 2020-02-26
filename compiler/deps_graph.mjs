1284f342947c5623767444db86532b3d8b922054
Craig Taub <craigtaub@gmail.com>
2020-02-26 18:04:31 +0000

    more tidy

--- compiler/deps_graph.mjs
+++ compiler/deps_graph.mjs
@@ -4,20 +4,20 @@ import ast from "abstract-syntax-tree";
 
 const depsArray = [];
 
-const depsGraph = (file, firstRun = false) => {
+const depsGraph = file, firstRun = false => {
   // TODO: locally doesnt add /src/ so needs it. relates to CWD.
   const fullPath = path.resolve(firstRun ? file : file.replace("./", "./src/"));
 
   // return early if exists
   if (!!depsArray.find(item => item.name === fullPath)) return;
 
-  // store path + source as module
-  const module = { name: fullPath };
-
-  // parse file source
+  // store path + parsed source as module
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const source = ast.parse(fileContents);
-  module.source = source;
+  const module = {
+    name: fullPath,
+    source
+  };
 
   // process deps
   source.body.map(current => {
